8260d9e4d7d59d567a05f1b3af41445720c224e6
Junaid Ali <junaidali@plumgrid.com>
2016-03-14 19:58:46 +0500

    Updated config-changed hook and restart_pg()

--- hooks/pg_dir_hooks.py
+++ hooks/pg_dir_hooks.py
@@ -14,8 +14,6 @@ from charmhelpers.core.hookenv import (
     config,
 )
 
-from charmhelpers.contrib.network.ip import is_ip
-
 from charmhelpers.fetch import (
     apt_install,
     apt_purge,
@@ -69,46 +67,35 @@ def config_changed():
     It also runs on node reboot.
     '''
     charm_config = config()
-    count = 0
-    for key in charm_config:
-        if charm_config.changed(key):
-            count += 1
     if charm_config.changed('lcm-ssh-key'):
         if add_lcm_key():
-            count -= 1
             log("PLUMgrid LCM Key added")
     if charm_config.changed('plumgrid-license-key'):
         if post_pg_license():
-            count -= 1
             log("PLUMgrid License Posted")
     if charm_config.changed('network-device-mtu'):
-        count -= 1
         ensure_mtu()
-    if count > 0:
-        stop_pg()
-    else:
-        CONFIGS.write_all()
-        return 1
-    if charm_config.changed('plumgrid-virtual-ip'):
-        if not is_ip(charm_config['plumgrid-virtual-ip']):
-            raise ValueError('Invalid IP Provided')
     if charm_config.changed('fabric-interfaces'):
         if not fabric_interface_changed():
             log("Fabric interface already set")
+        else:
+            restart_pg()
     if charm_config.changed('os-data-network'):
         if charm_config['fabric-interfaces'] == 'MANAGEMENT':
             log('Fabric running on managment network')
     if (charm_config.changed('install_sources') or
         charm_config.changed('plumgrid-build') or
+        charm_config.changed('plumgrid-virtual-ip') or
             charm_config.changed('iovisor-build')):
+        stop_pg()
         configure_sources(update=True)
         pkgs = determine_packages()
         for pkg in pkgs:
             apt_install(pkg, options=['--force-yes'], fatal=True)
             remove_iovisor()
             load_iovisor()
+        restart_pg()
     CONFIGS.write_all()
-    restart_pg()
 
 
 @hooks.hook('start')
